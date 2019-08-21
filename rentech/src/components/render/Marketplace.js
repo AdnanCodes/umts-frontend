8225ffb9f9709dfc150fb321ebc8105dce058a03
Adnan <adnan.chowdhury@outlook.com>
2019-08-21 12:42:09 -0500

    test change

--- rentech/src/components/render/Marketplace.js
+++ rentech/src/components/render/Marketplace.js
@@ -1,33 +1,31 @@
-import React, {useEffect} from "react";
-import {connect} from 'react-redux'
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
 import ItemCard from "./ItemCard";
-import  {getEquipmentList} from '../../store/actions/equipmentActions'
+import { getEquipmentList } from "../../store/actions/equipmentActions";
 //This component is rendering elements for 'Explore Tech'
 
-
 const Marketplace = props => {
-
-useEffect(() => {
-  props.getEquipmentList()
-},[]);
-
+  useEffect(() => {
+    props.getEquipmentList();
+  }, []);
 
   return (
     <div className="items">
       <h1>Check out the RenTech Marketplace!</h1>
       <div className="item-list">
-        
-      {props.rentItems && props.rentItems.map(rentItem => (
-        <ItemCard key={rentItem.id } rentItem={rentItem}/>
-      ) )}
-        
+        {props.rentItems &&
+          props.rentItems.map(rentItem => (
+            <ItemCard key={rentItem.id} rentItem={rentItem} />
+          ))}
       </div>
     </div>
   );
 };
-const mapStateToProps = (state) => ({
-rentItems: state.equipmentReducer.rentItems,
+const mapStateToProps = state => ({
+  rentItems: state.equipmentReducer.rentItems
+});
 
-})
-
-export default connect(mapStateToProps, {getEquipmentList})(Marketplace);
+export default connect(
+  mapStateToProps,
+  { getEquipmentList }
+)(Marketplace);
